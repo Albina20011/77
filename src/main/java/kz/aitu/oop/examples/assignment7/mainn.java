890d8319ca9e7098775330de452e4d6c2019f413
Сабит <Aimuratova_A0127@mail.ru>
2020-04-28 22:22:12 +0500

    assignment 7

--- src/main/java/kz/aitu/oop/examples/assignment7/mainn.java
+++ src/main/java/kz/aitu/oop/examples/assignment7/mainn.java
@@ -4,6 +4,6 @@ public class mainn {
     public static void main(String[] args) {
                 Circle circle= new Circle("red", true, 1.0);
                 Rectangle rectangle= new Rectangle("red",true, 1.0, 1.0);
-                        Square square= new Square("red", true, 1.0, 1.0);
+                        Square square= new Square ("red", true, 1.0, 1.0);
     }
 }
